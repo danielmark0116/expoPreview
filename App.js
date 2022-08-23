94f0eeeab404f5987680f14feda593638a00d719
Daniel Grychtoł <44443723+danielmark0116@users.noreply.github.com>
2022-08-24 01:57:29 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.text}>Hello The Widlarz Group</Text>
+			<Text style={styles.text}>Hello The Widlarz Group df sdffd</Text>
 			<StatusBar style="auto" />
 		</View>
 	);
